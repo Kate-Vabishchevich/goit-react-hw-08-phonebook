a6801e532805f48d789090b011505af648ba4037
Macbook <108225103+Kate-Vabishchevich@users.noreply.github.com>
2023-04-05 11:18:37 +0300

    add pages

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { store, persistor } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
